be60cb18f62fc084ad4c1d0c6dcea30b8119f6c2
danielgjacobsen <daniel_jacobsen@sicredi.com.br>
2018-04-15 20:03:21 -0300

    ajustada mensagem de entrada ddo login

--- chartjs2/index.php
+++ chartjs2/index.php
@@ -12,6 +12,6 @@
     }else{
         echo"Bem-Vindo, convidado <br>";
             echo"Essas informações <font color='red'>NÃO PODEM</font> ser acessadas por você";
-            echo"<br><a href='login.html'>Faça Login</a> Para ler o conteúdo";
+            echo"<br><a href='login.html'>Faça Login</a> Para acessar o conteúdo";
     }
 ?>
