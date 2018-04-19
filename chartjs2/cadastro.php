f42e9ded23d11c984c72dbdbf90d6ed562bd374f
danielgjacobsen <daniel_jacobsen@sicredi.com.br>
2018-04-18 21:15:41 -0300

    ajustes par segurar execução por admin e cadasro ok

--- chartjs2/cadastro.php
+++ chartjs2/cadastro.php
@@ -1,26 +1,33 @@
 <?php 
-$login = $_POST['login'];
-$senha = MD5($_POST['senha']);
+$login = $_COOKIE['login'];
 $perfil = $_COOKIE['perfil'];
+$newlogin = $_POST['login'];
+$newsenha = MD5($_POST['senha']);
+$newperfil = $_POST['perfil'];
 $connect = mysqli_connect('localhost', 'root', '12qwaszx', 'selo');
-$query_select = "SELECT login FROM usuarios WHERE login = '$login'";
+$query_select = "SELECT login FROM usuarios WHERE login = '$newlogin'";
 $select = mysqli_query($connect, $query_select);
 $array = mysqli_fetch_array($select);
 $logarray = $array['login']; 
-if($login == "" || $login == null || $perfil == "" || $perfil == null || $senha == "" || $senha == null){
-	echo"<script language='javascript' type='text/javascript'>alert('Todos os campos devem ser preenchidos');window.location.href='cadastroForm.php';</script>";
-}else{
-        if($logarray == $login){
-	        echo"<script language='javascript' type='text/javascript'>alert('Esse login já existe');window.location.href='cadastroForm.php';</script>";
-	        die();
+if(isset($login_cookie)){
+	if($newlogin == "" || $newlogin == null || $newperfil == "" || $newperfil == null || $newsenha == "" || $newsenha == null){
+		echo"<script language='javascript' type='text/javascript'>alert('Todos os campos devem ser preenchidos');window.location.href='cadastroForm.php';</script>";
 	}else{
-	        $query = "INSERT INTO usuarios (login,senha, perfil) VALUES ('$login','$senha', '$perfil')";
-		$insert = mysqli_query($connect, $query);
-	        if($insert){
-	                echo"<script language='javascript' type='text/javascript'>alert('Usuário cadastrado com sucesso!');window.location.href='login.html'</script>";
-	        }else{
-		        echo"<script language='javascript' type='text/javascript'>alert('Não foi possível cadastrar esse usuário');window.location.href='cadastroFomr.php'</script>";
+		if($logarray == $newlogin){
+			echo"<script language='javascript' type='text/javascript'>alert('Esse login já existe');window.location.href='cadastroForm.php';</script>";
+			die();
+		}else{
+			$query = "INSERT INTO usuarios (login,senha, perfil) VALUES ('$newlogin','$newsenha', '$newperfil')";
+			$insert = mysqli_query($connect, $query);
+			if($insert){
+				echo"<script language='javascript' type='text/javascript'>alert('Usuário cadastrado com sucesso!');window.location.href='login.html'</script>";
+			}else{
+				echo"<script language='javascript' type='text/javascript'>alert('Não foi possível cadastrar esse usuário');window.location.href='cadastroFomr.php'</script>";
+			}
 		}
 	}
+}else{	
+	echo"<script language='javascript' type='text/javascript'>alert('Para acessar o sistema, faça login!');</script>";
+	header("Location:login.html");
 }
 ?>
