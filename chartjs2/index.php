77290077e2bd0bbde7583348740455dae8fb4dc0
danielgjacobsen <daniel_jacobsen@sicredi.com.br>
2018-04-19 20:42:45 -0300

    ajuste para ficar o php e html juntos

--- chartjs2/index.php
+++ chartjs2/index.php
@@ -2,15 +2,32 @@
     $login_cookie = $_COOKIE['login'];
     $perfil_cookie = $_COOKIE['perfil'];
     if(isset($login_cookie)){
-	    
-        if($perfil_cookie == "admin"){
-            include 'menu_admin.html';
-        }else{
-            include 'menu.html';
-        }
-    }else{
-	
+	echo"<script language='javascript' type='text/javascript'>alert('Bem vindo ao Sistema Selo!');</script>";
+    }else{	
         echo"<script language='javascript' type='text/javascript'>alert('Para acessar o sistema, faça login!');</script>";
         header("Location:login.html");
     }
 ?>
+<!doctype html>
+	<head>
+	   <meta charset='utf-8'>
+	   <meta name="viewport" content="width=device-width, initial-scale=1">
+	   <link rel="stylesheet" href="css/menu_styles.css">
+	   <title>Selo</title>
+	</head>
+	<body>
+		<div id='cssmenu'>
+			<ul>
+				<li class='active'><a href='index.php'>Home</a></li>
+				<?php
+				if($perfil_cookie == "admin"){
+					echo"<li><a href='cadastroForm.php'>Usu&aacute;rios</a></li>";
+				}
+				?>		
+			   	<li><a href='radar-db-php.html'>Gráficos</a></li>
+			   	<li><a href='#'>Company</a></li>
+				<li><a href='#'>Contact</a></li>
+			</ul>
+		</div>
+	</body>
+<html>
