de45cc776b47f15835f611519128c366d66698eb
danielgjacobsen <daniel_jacobsen@sicredi.com.br>
2018-04-21 19:36:52 -0300

    Create menu.php

---
+++ chartjs2/menu.php
@@ -0,0 +1,13 @@
+<div id='cssmenu'>
+  <ul>
+    <li class='active'><a href='index.php'>Home</a></li>
+    <?php
+    if($perfil_cookie == "admin"){
+      echo"<li><a href='cadastroForm.php'>Usu&aacute;rios</a></li>";
+    }
+    ?>		
+    <li><a href='#'>Novo</a></li>
+      <li><a href='radar-db-php.html'>Gráficosss</a></li>
+      <li><a href='logout.php'>Sair</a></li>
+  </ul>
+</div>
