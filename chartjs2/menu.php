4a59f089d28af32daa88fdabf14bcea81c2b1697
danielgjacobsen <daniel_jacobsen@sicredi.com.br>
2018-04-21 19:51:04 -0300

    ajuste para chamar a tela cadastroPessoaForm no mennu novo

--- chartjs2/menu.php
+++ chartjs2/menu.php
@@ -6,8 +6,7 @@
         echo"<li><a href='cadastroForm.php'>Usu&aacute;rios</a></li>";
       }
     ?>		
-    <li><a href='#'>Novo</a></li>
-    <li><a href='radar-db-php.html'>Gráficosss</a></li>
+    <li><a href='cadastroPessoaForm.php'>Novo</a></li>
     <li><a href='logout.php'>Sair</a></li>
   </ul>
 </div>
