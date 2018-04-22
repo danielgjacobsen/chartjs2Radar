a936fc7cb62cdd44d2990051c203d962be70f26e
danielgjacobsen <daniel_jacobsen@sicredi.com.br>
2018-04-21 21:38:58 -0300

    Update cadastroPessoaForm.php

--- chartjs2/cadastroPessoaForm.php
+++ chartjs2/cadastroPessoaForm.php
@@ -5,7 +5,9 @@
   $perfil_cookie = $_COOKIE['perfil'];
   if(isset($_COOKIE['time'])) {
     $time_cookie = $_COOKIE['time'];
-  }  
+  }else{
+    $time_cookie = "";
+  }
   if(!isset($login_cookie)){
     echo"<script language='javascript' type='text/javascript'>alert('Necessário realizar login para acessar esta funcionalidade!');window.location.href='login.html';</script>";
     die();
