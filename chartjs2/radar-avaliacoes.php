8a1eb67525ad5d332e3b187d3bc1f81514894f95
danielgjacobsen <daniel_jacobsen@sicredi.com.br>
2018-04-22 00:19:45 -0300

    add perfilcookie

--- chartjs2/radar-avaliacoes.php
+++ chartjs2/radar-avaliacoes.php
@@ -2,6 +2,7 @@
   ini_set('display_errors', true);
   error_reporting(E_ALL);
   $login_cookie = $_COOKIE['login'];
+  $perfil_cookie = $_COOKIE['perfil'];
   if(!isset($login_cookie)){
     echo"<script language='javascript' type='text/javascript'>alert('Necessário realizar login para acessar esta funcionalidade!');window.location.href='login.html';</script>";
     die();
