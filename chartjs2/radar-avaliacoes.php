f468f4529648b4fc2e83f507668f4f14cf53662a
danielgjacobsen <daniel_jacobsen@sicredi.com.br>
2018-04-22 00:17:49 -0300

    adicionado menu

--- chartjs2/radar-avaliacoes.php
+++ chartjs2/radar-avaliacoes.php
@@ -10,25 +10,22 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Radar de Avaliações Selo</title>
-    
-    <link href="css/default.css" rel="stylesheet">
-    
+    <title>Radar de Avaliações Selo</title>    
+    <link href="css/default.css" rel="stylesheet">    
 </head>
 <body>
-
+	<?php 
+		include 'menu.php';
+	?>
 	<div class="chart-container">
 		<div class="doughnut-chart-container">
 			<canvas id="radar-avaliacoes"></canvas>
 		</div>
-
 	</div>
-
 	<!-- javascript -->
-    <script src="js/jquery.min.js"></script>
-    <script src="js/Chart.min.js"></script>
-    <script src="js/utils.js"></script>
-    <script src="js/radar-avaliacoes-db-php.js"></script>
-    
+    	<script src="js/jquery.min.js"></script>
+    	<script src="js/Chart.min.js"></script>
+    	<script src="js/utils.js"></script>
+    	<script src="js/radar-avaliacoes-db-php.js"></script>    
 </body>
 </html>
