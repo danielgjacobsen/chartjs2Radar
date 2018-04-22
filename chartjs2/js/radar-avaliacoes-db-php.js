1f5018c1c9e63b0c3bb22c3d1f5690fb17e86862
danielgjacobsen <daniel_jacobsen@sicredi.com.br>
2018-04-22 00:06:41 -0300

    Update radar-avaliacoes-db-php.js

--- chartjs2/js/radar-avaliacoes-db-php.js
+++ chartjs2/js/radar-avaliacoes-db-php.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	 * call the data.php file to fetch the result from db table.
 	 */
 	$.ajax({
-		url : "http://35.224.227.85/chartjs2/api/data.php",
+		url : "http://35.224.227.85/chartjs2/api/data-avaliacoes.php",
 		type : "GET",
 		success : function(data){
 			console.log(data);
