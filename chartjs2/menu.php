c330825acc1555a7f6e03a535891f3f0189fc4b3
danielgjacobsen <daniel_jacobsen@sicredi.com.br>
2018-04-23 19:51:33 -0300

    Update menu.php

--- chartjs2/menu.php
+++ chartjs2/menu.php
@@ -10,7 +10,7 @@
         echo"<li><a href='cadastroForm.php'>Usu&aacute;rios</a></li>";
       }
     ?>		
-    <li <?php if($menuSel=='Novo'){ echo"class='active'"; } ?>><a href='cadastroPessoaForm.php' onclick='setcookie("menuSel","Novo");'>Novo</a></li>
+    <li <?php if($menuSel=='Novo'){ echo"class='active'"; } ?>><a href='cadastroPessoaForm.php' onclick='document.cookie = "menuSel=Novo"'>Novo</a></li>
     <li ><a href='graficos.php'>Gr&aacute;ficos</a></li>
     <li><a href='logout.php'>Sair</a></li>
   </ul>
