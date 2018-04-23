d6f2156d026834f2509cc06f539995ccc68b0503
danielgjacobsen <daniel_jacobsen@sicredi.com.br>
2018-04-23 19:55:33 -0300

    Update menu.php

--- chartjs2/menu.php
+++ chartjs2/menu.php
@@ -4,14 +4,14 @@
 
 <div id='cssmenu'>
   <ul>
-    <li <?php if($menuSel=='Home'){ echo"class='active'"; } ?>><a href='index.php'>Home</a></li>
+    <li <?php if($menuSel=='Home'){ echo"class='active'"; } ?>><a href='index.php' onclick='document.cookie = "menuSel=Home"'>Home</a></li>
     <?php
       if($perfil_cookie == "admin"){
         echo"<li><a href='cadastroForm.php'>Usu&aacute;rios</a></li>";
       }
     ?>		
     <li <?php if($menuSel=='Novo'){ echo"class='active'"; } ?>><a href='cadastroPessoaForm.php' onclick='document.cookie = "menuSel=Novo"'>Novo</a></li>
-    <li ><a href='graficos.php'>Gr&aacute;ficos</a></li>
+    <li <?php if($menuSel=='Graficos'){ echo"class='active'"; } ?>><a href='graficos.php' onclick='document.cookie = "menuSel=Graficos"'>Gr&aacute;ficos</a></li>
     <li><a href='logout.php'>Sair</a></li>
   </ul>
 </div>
