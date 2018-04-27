9ad512b94f3b417b0c028f77dc1cb693dceff5c1
danielgjacobsen <daniel_jacobsen@sicredi.com.br>
2018-04-27 20:41:55 -0300

    Update edicaoPessoaSelect.php

--- chartjs2/edicaoPessoaSelect.php
+++ chartjs2/edicaoPessoaSelect.php
@@ -23,11 +23,34 @@
 ?>
 <!doctype html>
 	<head>
-	   <meta charset='utf-8'>
-	   <meta name="viewport" content="width=device-width, initial-scale=1">
-	   <link rel="stylesheet" href="css/menu_styles.css">
-	   <link rel="stylesheet" href="css/formulario.css">
-	   <title>Selo</title>
+		<meta charset='utf-8'>
+	   	<meta name="viewport" content="width=device-width, initial-scale=1">
+	   	<link rel="stylesheet" href="css/menu_styles.css">
+	   	<link rel="stylesheet" href="css/formulario.css">
+	   	<title>Selo</title>
+	   	<script>
+			function showPessoas(str) {
+			    if (str == "") {
+				document.getElementById("txtHint").innerHTML = "";
+				return;
+			    } else { 
+				if (window.XMLHttpRequest) {
+				    // code for IE7+, Firefox, Chrome, Opera, Safari
+				    xmlhttp = new XMLHttpRequest();
+				} else {
+				    // code for IE6, IE5
+				    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+				}
+				xmlhttp.onreadystatechange = function() {
+				    if (this.readyState == 4 && this.status == 200) {
+					document.getElementById("txtHint").innerHTML = this.responseText;
+				    }
+				};
+				xmlhttp.open("GET","getpessoas.php?q="+str,true);
+				xmlhttp.send();
+			    }
+			}
+	    	</script>
 	</head>
 	<body>
 		<?php 
@@ -36,11 +59,11 @@
 		<form action="cadastroPessoa.php" method="post">
 			<fieldset>
 				<div class="campo">
-					<select name="time" id="time">
+					<select name="time" id="time" onchange="showPessoas(this.value)">
 				      		<?php echo $optionTime; ?>
 				  	</select>
 				</div>
-				
+				<div class="campo" id="txtHint"><b>Selecione o time para abrir a lista das pessoas...</b></div>
 				<button type="submit" name="submit">Salvar</button>
 			</fieldset>
 		</form>
