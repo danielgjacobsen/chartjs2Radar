235b03ce6181c0b6cd789914cae55b24b9939f61
danielgjacobsen <daniel_jacobsen@sicredi.com.br>
2018-04-27 20:23:57 -0300

    Update edicaoPessoaSelect.php

--- chartjs2/edicaoPessoaSelect.php
+++ chartjs2/edicaoPessoaSelect.php
@@ -12,59 +12,7 @@
     echo"<script language='javascript' type='text/javascript'>alert('Necessário realizar login para acessar esta funcionalidade!');window.location.href='login.html';</script>";
     die();
   }
-  $connect = mysqli_connect('localhost', 'root', '12qwaszx', 'selo');
-  $query = "SELECT time FROM avaliacoes WHERE login = '$login_cookie' group by time order by time";
-  $select = mysqli_query($connect, $query);
-  $optionTime = '';
-  while($row = mysqli_fetch_assoc($select)){
-    $optionTime .= '<option value = "'.$row['time'].'">'.$row['time'].'</option>';
-  }
-  mysqli_close($connect);
-  function showPessoas(str) {
-    if (str == "") {
-        document.getElementById("txtHint").innerHTML = "";
-        return;
-    } else { 
-        if (window.XMLHttpRequest) {
-            // code for IE7+, Firefox, Chrome, Opera, Safari
-            xmlhttp = new XMLHttpRequest();
-        } else {
-            // code for IE6, IE5
-            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById("txtHint").innerHTML = this.responseText;
-            }
-        };
-        xmlhttp.open("GET","getpessoas.php?q="+str,true);
-        xmlhttp.send();
-    }
+  
 }
 ?>
-<!doctype html>
-	<head>
-	   <meta charset='utf-8'>
-	   <meta name="viewport" content="width=device-width, initial-scale=1">
-	   <link rel="stylesheet" href="css/menu_styles.css">
-	   <link rel="stylesheet" href="css/formulario.css">
-	   <title>Selo</title>
-	</head>
-	<body>
-		<?php 
-			include 'menu.php';
-		?>
-		<form action="edicaoPessoaForm.php" method="post">
-			<fieldset>
-				<div class="campo">
-					<label>Time</label>
-					<select name="time" id="time" onchange="showPessoas(this.value)">
-					    <?php echo $optionTime; ?>
-					</select>
-				</div>
-				<div class="campo" id="txtHint"><b>Selecione o time para abrir a lista das pessoas...</b></div>
-				<button type="submit" name="submit">Editar</button>
-			</fieldset>
-		</form>
-	</body>
-<html>
+
