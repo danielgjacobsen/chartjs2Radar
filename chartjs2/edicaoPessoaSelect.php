1f86adbb6fbfc927a8aa4fd16249fe4ba922b474
danielgjacobsen <daniel_jacobsen@sicredi.com.br>
2018-04-27 20:48:14 -0300

    Update edicaoPessoaSelect.php

--- chartjs2/edicaoPessoaSelect.php
+++ chartjs2/edicaoPessoaSelect.php
@@ -30,7 +30,25 @@
 	   	<title>Selo</title>
 	   	<script>
 			function showPessoas(str) {
-			    alert(str);
+			    if (str == "") {
+				document.getElementById("txtHint").innerHTML = "";
+				return;
+			    } else { 
+				if (window.XMLHttpRequest) {
+				    // code for IE7+, Firefox, Chrome, Opera, Safari
+				    xmlhttp = new XMLHttpRequest();
+				} else {
+				    // code for IE6, IE5
+				    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+				}
+				xmlhttp.onreadystatechange = function() {
+				    if (this.readyState == 4 && this.status == 200) {
+					document.getElementById("txtHint").innerHTML = this.responseText;
+				    }
+				};
+				xmlhttp.open("GET","getpessoas.php?q="+str,true);
+				xmlhttp.send();
+			    }
 			}
 	    	</script>
 	</head>
