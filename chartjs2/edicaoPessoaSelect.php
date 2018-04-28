f8d576b49f4d61d47993f1bab04fc88ab0bbd1ab
danielgjacobsen <daniel_jacobsen@sicredi.com.br>
2018-04-28 00:56:44 -0300

    adicionada getavaliacao

--- chartjs2/edicaoPessoaSelect.php
+++ chartjs2/edicaoPessoaSelect.php
@@ -51,7 +51,25 @@
 			    }
 			}
 			function showAvaliacao(str) {
-			    alert(str);
+			    if (str == "") {
+				document.getElementById("txtAval").innerHTML = "";
+				return;
+			    } else { 
+				if (window.XMLHttpRequest) {
+				    // code for IE7+, Firefox, Chrome, Opera, Safari
+				    xmlhttp = new XMLHttpRequest();
+				} else {
+				    // code for IE6, IE5
+				    xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+				}
+				xmlhttp.onreadystatechange = function() {
+				    if (this.readyState == 4 && this.status == 200) {
+					document.getElementById("txtAval").innerHTML = this.responseText;
+				    }
+				};
+				xmlhttp.open("GET","getavaliacao.php?q="+str,true);
+				xmlhttp.send();
+			    }
 			}
 	    	</script>
 	</head>
