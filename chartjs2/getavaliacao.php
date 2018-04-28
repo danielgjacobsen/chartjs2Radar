0ba5b4443e4408aea48c2ad11f19a5a1f30a4a9d
danielgjacobsen <daniel_jacobsen@sicredi.com.br>
2018-04-28 02:44:50 -0300

    Update getavaliacao.php

--- chartjs2/getavaliacao.php
+++ chartjs2/getavaliacao.php
@@ -6,13 +6,9 @@ $connect = mysqli_connect('localhost', 'root', '12qwaszx', 'selo');
 $query = "SELECT * FROM avaliacoes WHERE login = '$login_cookie' and time = '$t' and pessoa = '$p' ";
 $select = mysqli_query($connect, $query);
 
-echo "<div class="campo">
-        <label for="time">Time</label>
-        <input type="text" id="time" name="time" style="width: 500px" value="<?php echo $time_cookie;?>">
-</div>";
-$row = mysqli_fetch_row($select)
+
+$row = mysqli_fetch_row($select);
   echo "<option value = " . $row['pessoa'] . ">" . $row['pessoa'] . "</option>";
-echo "</select>";
 
 mysqli_close($connect);
 ?>
