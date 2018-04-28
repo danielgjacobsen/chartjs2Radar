55ef4b06b2e4bdba3049e4176e9fb3631ce647e0
danielgjacobsen <daniel_jacobsen@sicredi.com.br>
2018-04-28 00:20:58 -0300

    Update getpessoas.php

--- chartjs2/getpessoas.php
+++ chartjs2/getpessoas.php
@@ -1,6 +1,15 @@
 <?php
-  $q = strval($_GET['q']);
+$q = strval($_GET['q']);
+$login_cookie = $_COOKIE['login'];
+$connect = mysqli_connect('localhost', 'root', '12qwaszx', 'selo');
+$query = "SELECT pessoa FROM avaliacoes WHERE login = '$login_cookie' and time = '$q' group by pessoa order by pessoa";
+$select = mysqli_query($connect, $query);
 
-
-echo $q;
+echo "<label>Pessoa</label>
+<select name='pessoa' id='pessoa'>
+while($row = mysqli_fetch_array($select)) {
+  echo "<option value = " . $row['pessoa'] . ">" . $row['pessoa'] . "</option>";
+}
+echo "</select>";
+mysqli_close($connect);
 ?>
