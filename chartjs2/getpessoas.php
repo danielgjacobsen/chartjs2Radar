88a77dbe810e43287ffc3d0221f5d51a086246f7
danielgjacobsen <daniel_jacobsen@sicredi.com.br>
2018-04-28 03:03:07 -0300

    Update getpessoas.php

--- chartjs2/getpessoas.php
+++ chartjs2/getpessoas.php
@@ -4,11 +4,11 @@ $login_cookie = $_COOKIE['login'];
 $connect = mysqli_connect('localhost', 'root', '12qwaszx', 'selo');
 $query = "SELECT pessoa FROM avaliacoes WHERE login = '$login_cookie' and time = '$q' group by pessoa order by pessoa";
 $select = mysqli_query($connect, $query);
-echo "<label>Pessoa</label>	
+echo "<label>Pessoa</label>
         <select name='pessoa' id='pessoa' style='width: 500px' onchange='showAvaliacao(this.value)'>	
-        <option value=''>Selecione a pessoa para editar</option>";	
+        <option value=''>Selecione a pessoa para editar</option>";
 while($row = mysqli_fetch_array($select)) {
-  echo "<option value = " . $row['pessoa'] . ">" . $row['pessoa'] . "</option>";	+  echo "<div class="campo">
+  echo "<option value = " . $row['pessoa'] . ">" . $row['pessoa'] . "</option>";
 }
 echo "</select>";
 mysqli_close($connect);
