b8c55d4675074e3d0aaedd45f9606cee5e62f140
danielgjacobsen <daniel_jacobsen@sicredi.com.br>
2018-04-28 02:40:58 -0300

    Update getavaliacao.php

--- chartjs2/getavaliacao.php
+++ chartjs2/getavaliacao.php
@@ -6,12 +6,12 @@ $connect = mysqli_connect('localhost', 'root', '12qwaszx', 'selo');
 $query = "SELECT * FROM avaliacoes WHERE login = '$login_cookie' and time = '$t' and pessoa = '$p' ";
 $select = mysqli_query($connect, $query);
 
-echo "<label>Pessoa</label>
-        <select name='pessoa' id='pessoa' style='width: 500px' onchange='showAvaliacao(this.value)'>
-        <option value=''>Selecione a pessoa para editar</option>";
-while($row = mysqli_fetch_array($select)) {
+echo "<div class="campo">
+        <label for="time">Time</label>
+        <input type="text" id="time" name="time" style="width: 500px" value="<?php echo $time_cookie;?>">
+</div>";
+$row = mysqli_fetch_row($select)
   echo "<option value = " . $row['pessoa'] . ">" . $row['pessoa'] . "</option>";
-}
 echo "</select>";
 
 mysqli_close($connect);
