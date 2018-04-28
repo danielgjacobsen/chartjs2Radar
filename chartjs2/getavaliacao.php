4aa52c117b03471e9e3f70b794c338940f92f115
danielgjacobsen <daniel_jacobsen@sicredi.com.br>
2018-04-28 02:20:02 -0300

    Update getavaliacao.php

--- chartjs2/getavaliacao.php
+++ chartjs2/getavaliacao.php
@@ -5,9 +5,14 @@ $login_cookie = $_COOKIE['login'];
 $connect = mysqli_connect('localhost', 'root', '12qwaszx', 'selo');
 $query = "SELECT * FROM avaliacoes WHERE login = '$login_cookie' and time = '$t' and pessoa = '$p' ";
 $select = mysqli_query($connect, $query);
+
+echo "<label>Pessoa</label>
+        <select name='pessoa' id='pessoa' style='width: 500px' onchange='showAvaliacao(this.value)'>
+        <option value=''>Selecione a pessoa para editar</option>";
 while($row = mysqli_fetch_array($select)) {
-  echo "$row['pessoa']";
+  echo "<option value = " . $row['pessoa'] . ">" . $row['pessoa'] . "</option>";
 }
+echo "</select>";
 
 mysqli_close($connect);
 ?>
