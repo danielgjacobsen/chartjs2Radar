c8afe040845ef448e26b9b370b253b034e2b3348
danielgjacobsen <daniel_jacobsen@sicredi.com.br>
2018-04-27 23:17:48 -0300

    Update getpessoas.php

--- chartjs2/getpessoas.php
+++ chartjs2/getpessoas.php
@@ -1,19 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-<style>
-table {
-    width: 100%;
-    border-collapse: collapse;
-}
 
-table, td, th {
-    border: 1px solid black;
-    padding: 5px;
-}
-
-th {text-align: left;}
-</style>
 </head>
 <body>
 
@@ -23,14 +11,16 @@ $q = intval($_GET['q']);
 $connect = mysqli_connect('localhost', 'root', '12qwaszx', 'selo');
 $query = "SELECT pessoa FROM avaliacoes WHERE login = '$login_cookie' and time = '".$q."' group by pessoa order by pessoa";
 $select = mysqli_query($connect, $query);
-$optionPessoa = '';
+$option = '';
 while($row = mysqli_fetch_assoc($select)){
-    $optionPessoa .= '<option value = "'.$row['pessoa'].'">'.$row['pessoa'].'</option>';
+$option .= '<option value = "'.$row['pessoa'].'">'.$row['pessoa'].'</option>';
 }
-
-echo "<label>Time</label>
-<select name="time" id="time">
-$option
-</select>";
 mysqli_close($connect);
+
+echo "<label>Pessoa</label>
+      <select name="pessoa" id="pessoa">
+          <?php echo $option; ?>
+      </select>";
 ?>
+</body>
+</html>
