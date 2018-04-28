fd7c3aa2146d4129316f9926433972808079d5b4
danielgjacobsen <daniel_jacobsen@sicredi.com.br>
2018-04-27 23:27:05 -0300

    Update getpessoas.php

--- chartjs2/getpessoas.php
+++ chartjs2/getpessoas.php
@@ -5,22 +5,14 @@
 </head>
 <body>
 
-<?php
-$q = intval($_GET['q']);
-
-$connect = mysqli_connect('localhost', 'root', '12qwaszx', 'selo');
-$query = "SELECT pessoa FROM avaliacoes WHERE login = '$login_cookie' and time = '".$q."' group by pessoa order by pessoa";
-$select = mysqli_query($connect, $query);
-$option = '';
-while($row = mysqli_fetch_assoc($select)){
-$option .= '<option value = "'.$row['pessoa'].'">'.$row['pessoa'].'</option>';
-}
-mysqli_close($connect);
-
-echo "<label>Pessoa</label>
-      <select name="pessoa" id="pessoa">
-          <?php echo $option; ?>
-      </select>";
-?>
+<table>
+<tr>
+<th>Firstname</th>
+<th>Lastname</th>
+<th>Age</th>
+<th>Hometown</th>
+<th>Job</th>
+</tr>
+</table>
 </body>
 </html>
