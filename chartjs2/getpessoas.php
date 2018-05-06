70ccec8442eafbdf53719bae0021baec41e3f37d
danielgjacobsen <daniel_jacobsen@sicredi.com.br>
2018-05-06 12:00:58 -0300

    ajustado nome do select de pessoa para passar para o update

--- chartjs2/getpessoas.php
+++ chartjs2/getpessoas.php
@@ -5,7 +5,7 @@ $connect = mysqli_connect('localhost', 'root', '12qwaszx', 'selo');
 $query = "SELECT pessoa FROM avaliacoes WHERE login = '$login_cookie' and time = '$q' group by pessoa order by pessoa";
 $select = mysqli_query($connect, $query);
 echo "<label>Pessoa</label>
-        <select name='pessoa' id='pessoa' style='width: 500px' onchange='showAvaliacao(this.value)'>	
+        <select name='selPessoa' id='selPessoa' style='width: 500px' onchange='showAvaliacao(this.value)'>	
         <option value=''>Selecione a pessoa para editar</option>";
 while($row = mysqli_fetch_array($select)) {
   echo "<option value = " . $row['pessoa'] . ">" . $row['pessoa'] . "</option>";
