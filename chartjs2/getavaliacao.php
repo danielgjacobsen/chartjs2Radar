80afdff9d54009be58acd026f7832912dd0a08d7
danielgjacobsen <daniel_jacobsen@sicredi.com.br>
2018-05-06 23:21:01 -0300

    alterado tipo dos inputs para nunber

--- chartjs2/getavaliacao.php
+++ chartjs2/getavaliacao.php
@@ -37,83 +37,83 @@ while($row = mysqli_fetch_array($select)) {
         </div>
         <div class='campo'>
 					<label>Agressividade</label>
-					<input type='text' id='Agressividade' name='Agressividade' style='width: 50px' value=" . $row['Agressividade'] . ">
+					<input type='number' id='Agressividade' name='Agressividade' style='width: 50px' value=" . $row['Agressividade'] . ">
 				</div>
 				<div class='campo'>
 					<label>Desenv. Relacionamento</label>
-					<input type='text' id='Desenv_Relacionamento' name='Desenv_Relacionamento' style='width: 50px' value=" . $row['Desenv_Relacionamento'] . ">
+					<input type='number' id='Desenv_Relacionamento' name='Desenv_Relacionamento' style='width: 50px' value=" . $row['Desenv_Relacionamento'] . ">
 				</div>
 				<div class='campo'>
 					<label>Facilidade com Mudan&ccedil;as</label>
-					<input type='text' id='Facilidade_Mudancas' name='Facilidade_Mudancas' style='width: 50px' value=" . $row['Facilidade_Mudancas'] . ">
+					<input type='number' id='Facilidade_Mudancas' name='Facilidade_Mudancas' style='width: 50px' value=" . $row['Facilidade_Mudancas'] . ">
 				</div>
 				<div class='campo'>
 					<label>Extrovers&atilde;o</label>
-					<input type='text' id='Extroversao' name='Extroversao' style='width: 50px' value=" . $row['Extroversao'] . ">
+					<input type='number' id='Extroversao' name='Extroversao' style='width: 50px' value=" . $row['Extroversao'] . ">
 				</div>
 				<div class='campo'>
 					<label>Domin&acirc;ncia</label>
-					<input type='text' id='Dominancia' name='Dominancia' style='width: 50px' value=" . $row['Dominancia'] . ">
+					<input type='number' id='Dominancia' name='Dominancia' style='width: 50px' value=" . $row['Dominancia'] . ">
 				</div>
 				<div class='campo'>
 					<label>Desenv. Trabalho</label>
-					<input type='text' id='Desenv_Trab' name='Desenv_Trab' style='width: 50px' value=" . $row['Desenv_Trab'] . ">
+					<input type='number' id='Desenv_Trab' name='Desenv_Trab' style='width: 50px' value=" . $row['Desenv_Trab'] . ">
 				</div>
 				<div class='campo'>
 					<label>Formalidade</label>
-					<input type='text' id='Formalidade' name='Formalidade' style='width: 50px' value=" . $row['Formalidade'] . ">
+					<input type='number' id='Formalidade' name='Formalidade' style='width: 50px' value=" . $row['Formalidade'] . ">
 				</div>
 				<div class='campo'>
 					<label>Condescend&ecirc;ncia</label>
-					<input type='text' id='Condescendencia' name='Condescendencia' style='width: 50px' value=" . $row['Condescendencia'] . ">
+					<input type='number' id='Condescendencia' name='Condescendencia' style='width: 50px' value=" . $row['Condescendencia'] . ">
 				</div>
 				<div class='campo'>
 					<label>Perfil T&eacute;cnico</label>
-					<input type='text' id='Perfil_Tecnico' name='Perfil_Tecnico' style='width: 50px' value=" . $row['Perfil_Tecnico'] . ">
+					<input type='number' id='Perfil_Tecnico' name='Perfil_Tecnico' style='width: 50px' value=" . $row['Perfil_Tecnico'] . ">
 				</div>
 				<div class='campo'>
 					<label>Exatid&atilde;o</label>
-					<input type='text' id='Exatidao' name='Exatidao' style='width: 50px' value=" . $row['Exatidao'] . ">
+					<input type='number' id='Exatidao' name='Exatidao' style='width: 50px' value=" . $row['Exatidao'] . ">
 				</div>
 				<div class='campo'>
 					<label>Detalhismo</label>
-					<input type='text' id='Detalhismo' name='Detalhismo' style='width: 50px' value=" . $row['Detalhismo'] . ">
+					<input type='number' id='Detalhismo' name='Detalhismo' style='width: 50px' value=" . $row['Detalhismo'] . ">
 				</div>
 				<div class='campo'>
 					<label>Perfil Art&iacute;stico</label>
-					<input type='text' id='Perfil_Artistico' name='Perfil_Artistico' style='width: 50px' value=" . $row['Perfil_Artistico'] . ">
+					<input type='number' id='Perfil_Artistico' name='Perfil_Artistico' style='width: 50px' value=" . $row['Perfil_Artistico'] . ">
 				</div>
 				<div class='campo'>
 					<label>Paci&ecirc;ncia</label>
-					<input type='text' id='Paciencia' name='Paciencia' style='width: 50px' value=" . $row['Paciencia'] . ">
+					<input type='number' id='Paciencia' name='Paciencia' style='width: 50px' value=" . $row['Paciencia'] . ">
 				</div>
 				<div class='campo'>
 					<label>Empatia</label>
-					<input type='text' id='Empatia' name='Empatia' style='width: 50px' value=" . $row['Empatia'] . ">
+					<input type='number' id='Empatia' name='Empatia' style='width: 50px' value=" . $row['Empatia'] . ">
 				</div>
 				<div class='campo'>
 					<label>Sociabilidade</label>
-					<input type='text' id='Sociabilidade' name='Sociabilidade' style='width: 50px' value=" . $row['Sociabilidade'] . ">
+					<input type='number' id='Sociabilidade' name='Sociabilidade' style='width: 50px' value=" . $row['Sociabilidade'] . ">
 				</div>
 				<div class='campo'>
 					<label>Entusiasmo</label>
-					<input type='text' id='Entusiasmo' name='Entusiasmo' style='width: 50px' value=" . $row['Entusiasmo'] . ">
+					<input type='number' id='Entusiasmo' name='Entusiasmo' style='width: 50px' value=" . $row['Entusiasmo'] . ">
 				</div>
 				<div class='campo'>
 					<label>Capacidade de Sonhar</label>
-					<input type='text' id='Cap_Sonhar' name='Cap_Sonhar' style='width: 50px' value=" . $row['Cap_Sonhar'] . ">
+					<input type='number' id='Cap_Sonhar' name='Cap_Sonhar' style='width: 50px' value=" . $row['Cap_Sonhar'] . ">
 				</div>
 				<div class='campo'>
 					<label>Automotiva&ccedil;&atilde;o</label>
-					<input type='text' id='Automotivacao' name='Automotivacao' style='width: 50px' value=" . $row['Automotivacao'] . ">
+					<input type='number' id='Automotivacao' name='Automotivacao' style='width: 50px' value=" . $row['Automotivacao'] . ">
 				</div>
 				<div class='campo'>
 					<label>Concentra&ccedil;&atilde;o</label>
-					<input type='text' id='Concentracao' name='Concentracao' style='width: 50px' value=" . $row['Concentracao'] . ">
+					<input type='number' id='Concentracao' name='Concentracao' style='width: 50px' value=" . $row['Concentracao'] . ">
 				</div>
 				<div class='campo'>
 					<label>Independ&ecirc;ncia</label>
-					<input type='text' id='Independencia' name='Independencia' style='width: 50px' value=" . $row['Independencia'] . ">
+					<input type='number' id='Independencia' name='Independencia' style='width: 50px' value=" . $row['Independencia'] . ">
 				</div>
 				<button type='submit' name='submit'>Salvar</button>";
 }
